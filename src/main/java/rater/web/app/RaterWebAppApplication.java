25f8bca4eca3cb930848c4ec1f6c4113fa8d3914
Alex Quesada <a.quemen@gmail.com>
2020-04-06 12:52:07 +0200

    init commit

---
+++ src/main/java/rater/web/app/RaterWebAppApplication.java
@@ -0,0 +1,13 @@
+package rater.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaterWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaterWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rater/web/app/RaterWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rater.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaterWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
