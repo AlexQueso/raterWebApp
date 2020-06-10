7c4994b47392a51e368e52b40ec796c0062a2b15
Alex <a.quemen@gmail.com>
2020-06-10 13:07:12 +0200

    update: error controller redirect user to main page

---
+++ src/main/java/rater/web/app/controllers/ErrorController.java
@@ -0,0 +1,12 @@
+package rater.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import rater.web.app.utils.Utils;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @Override
+    public String getErrorPath() {
+        return Utils.redirectTo("/log-out");
+    }
+}
